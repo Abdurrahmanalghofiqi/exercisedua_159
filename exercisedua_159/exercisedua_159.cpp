cf0a42f1a2c84e3f145e8a187bab8b187e6f1019
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-06-05 14:55:56 +0700

    Add project files.

---
+++ exercisedua_159/exercisedua_159.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef JOKO_P
+#define JOKO_P
+
+class joko {
+public:
+	string nama;
+	joko(string pNama) : nama(pNama) {
+		cout << "Joko \"" << nama << "\"ada\n";
+	}
+	~joko() {
+		cout << "Joko \"" << nama << "\" tidak ada\n";
+	}
+};
+void tambahan{
+int main() {
+	pengarang* varPengarang1 = new adi("penerbit");
+	pengarang* varPengarang2 = new adu("pengarang");
+	penerbit* varPenerbit1 = new pengarang("adu");
+	Penerbit* varPenerbit2 = new pengarang("adu");
+
+	varPengarang->tambahpenerbit(varVenerbit1);
+	varPengarang->tambahpenerbit(varPenerbit);
+	varPengarang->tambahpenerbit(varPenerbit);
+	}
+}
